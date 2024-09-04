304ddae62ad09a772fab5aee8026d6f77797f551
Krishnasinh Jadeja <113630783+K-Jadeja@users.noreply.github.com>
2024-09-04 23:19:37 +0530

    Update Watermarking.jsx

--- src/pages/Watermarking.jsx
+++ src/pages/Watermarking.jsx
@@ -22,7 +22,7 @@ function Watermarking() {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-r from-blue-100 to-purple-100 py-20 px-6">
+    <div className="min-h-screen bg-gradient-to-r from-blue-100 to-purple-100 py-40 px-6">
       <div className="max-w-4xl mx-auto text-center mt-15">
         <h2 className="text-4xl font-bold text-gray-800 mb-8">Add Watermark</h2>
         <p className="text-lg text-gray-600 mb-8">
